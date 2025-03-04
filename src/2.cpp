6bfd2b8de681501177b2a82ef9d9e7190f9b7bc0
mikide3501 <miki.de@gmx.com>
2025-03-04 13:51:53 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,18 @@
+// Randomly generated C++ code
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "Alice";
+    int age = 30;
+    bool isMarried = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Is Married: " << isMarried << endl;
+
+    return 0;
+}
